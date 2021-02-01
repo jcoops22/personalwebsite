e3294bfbb9f3b8a4b3032dfff4f6dc7cbb22d038
jcoops22 <jcooper232074@gmail.com>
2021-02-01 12:53:58 -0700

    edited the phew apparel work info

--- src/Components/Intro/BackgroundHeader.js
+++ src/Components/Intro/BackgroundHeader.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { device } from "../../resources/mediaquery";
 
 const BackgroundHeader = ({ text }) => {
+  useEffect(() => {
+    // window.addEventListener("scroll", parrallax);
+  });
+
+  // parallax effect
+  const parrallax = () => {
+    const background = document.querySelectorAll(".bg_header_wrapper");
+    let scrolled = window.pageYOffset;
+    background[0].style.top = scrolled * 0.5 + "px";
+    background[1].style.top = scrolled * 0.5 + "px";
+    background[2].style.top = scrolled * 0.5 + "px";
+    console.log(background);
+  };
+
   return (
-    <Wrapper>
+    <Wrapper className="bg_header_wrapper">
       <h1>{text}</h1>
     </Wrapper>
   );
